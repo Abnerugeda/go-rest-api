c9d41fb448a9c825060a217fa487ebc8d09c4d9e
abnerugeda <abner.ugeda@carefy.com.br>
2024-12-27 20:18:10 -0300

    feat: criando middleware para formatar json

--- routes/routes.go
+++ routes/routes.go
@@ -5,11 +5,13 @@ import (
 	"net/http"
 
 	"github.com/Abnerugeda/go-rest-api/controllers"
+	"github.com/Abnerugeda/go-rest-api/middleware"
 	"github.com/gorilla/mux"
 )
 
 func HandleRequest() {
 	r := mux.NewRouter()
+	r.Use(middleware.ContentTypeMiddleware)
 	r.HandleFunc("/", controllers.Home)
 	r.HandleFunc("/api/personalidades", controllers.TodasPersonalidades).Methods("Get")
 	r.HandleFunc("/api/personalidades/{id}", controllers.FindOnePersonalidade).Methods("Get")
